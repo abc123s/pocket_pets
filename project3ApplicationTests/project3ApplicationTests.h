08da61b9f7986f4c1aba0110afcbd9391ca134b6
Will Sun <wjy.sun@gmail.com>
2012-05-03 11:06:50 -0400

    app test fw

---
+++ project3ApplicationTests/project3ApplicationTests.h
@@ -0,0 +1,13 @@
+//
+//  project3ApplicationTests.h
+//  project3ApplicationTests
+//
+//  Created by Will Sun on 5/3/12.
+//  Copyright (c) 2012 Harvard University. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface project3ApplicationTests : SenTestCase
+
+@end
---
+++ project3ApplicationTests/project3ApplicationTests.m
@@ -0,0 +1,32 @@
+//
+//  project3ApplicationTests.m
+//  project3ApplicationTests
+//
+//  Created by Will Sun on 5/3/12.
+//  Copyright (c) 2012 Harvard University. All rights reserved.
+//
+
+#import "project3ApplicationTests.h"
+
+@implementation project3ApplicationTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in project3ApplicationTests");
+}
+
+@end
